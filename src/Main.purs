e69bc3a65ceefdde794ac2f1a8015711be11234e
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2016-11-05 22:58:08 -0600

    Add vector space instance for numbers.

--- src/Main.purs
+++ src/Main.purs
@@ -4,10 +4,15 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Monoid (class Monoid)
+import Data.Monoid.Additive (Additive(..))
 
 class (Monoid v, Field f) <= VectorSpace v f | v -> f where
   vecNegate :: v -> v
   scalarMult :: f -> v -> v
+
+instance vectorSpaceNumber :: VectorSpace (Additive Number) Number where
+  vecNegate (Additive x) = Additive (-x)
+  scalarMult y (Additive x) = Additive (x * y)
 
 type ScalarField v f = (VectorSpace v f) => v -> f
 type VectorField v f = (VectorSpace v f) => v -> v
